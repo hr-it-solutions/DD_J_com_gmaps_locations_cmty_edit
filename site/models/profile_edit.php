cff1ef1a97b8191615ef060521ea181fee562f0c
Florian Häusler <florian@didldu.com>
2017-10-31 22:23:28 +0100

    profile_edit model added

---
+++ site/models/profile_edit.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package    DD_GMaps_Locations
+ *
+ * @author     HR IT-Solutions Florian Häusler <info@hr-it-solutions.com>
+ * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+use Joomla\Registry\Registry;
+
+JLoader::register('DD_GMaps_LocationsHelper', JPATH_ADMINISTRATOR . '/components/com_dd_gmaps_locations/helpers/dd_gmaps_locations.php');
+
+/**
+ * Class DD_GMaps_LocationsModelLocation
+ *
+ * @since  Version 1.0.0.0
+ */
+class DD_GMaps_Locations_CMTY_EditModelProfile_Edit extends JModelItem
+{
+
+}
